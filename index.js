76f6abcffa0c2ba4b30c48f76b41544b9907544b
Alan Sanchez <alsans81@outlook.com>
2026-01-13 15:58:03 -0500

    Added routes for user, event, & booking

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const PORT = 3000;
 app.use(express.json())
 app.use(logger("dev"))
 
-const userRouter = require('./routes/users/userRouter')
+const userRouter = require('./routes/users/usersRouter')
 app.use('/api/user', userRouter)
+
+const eventRouter = require('./routes/events/eventsRouter')
+app.use('/api/event', eventRouter)
+
+const bookingRouter = require('./routes/bookings/bookingsRouter')
+app.use('/api/booking', bookingRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`);
